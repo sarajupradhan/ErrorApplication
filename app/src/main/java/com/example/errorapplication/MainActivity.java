31f127fdbc33c52c95b367536f15d72d78bdb3de
gp5855 <gagan.patil@zebra.com>
2025-06-25 12:39:50 +0530

    Handle FileNotFoundException by checking file existence before opening

--- app/src/main/java/com/example/errorapplication/MainActivity.java
+++ app/src/main/java/com/example/errorapplication/MainActivity.java
@@ -143,11 +143,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void simulateFileNotFoundException() {
-        try {
-            FileInputStream fis = new FileInputStream("non_existent_file.txt");
+        String fileName = "non_existent_file.txt";
+        File file = new File(getFilesDir(), fileName);
+        if (!file.exists()) {
+            Log.w(TAG, "File does not exist: " + file.getAbsolutePath());
+            Toast.makeText(this, getString(R.string.file_not_found_exception), Toast.LENGTH_SHORT).show();
+            writeErrorToFile(getString(R.string.file_not_found_exception) + ": " + file.getAbsolutePath(), null);
+            return;
+        }
+        try (FileInputStream fis = new FileInputStream(file)) {
+            // Simulate file reading or processing here if needed
         } catch (FileNotFoundException e) {
-            Log.e(TAG, getString(R.string.file_not_found_exception), e);
-            writeErrorToFile(getString(R.string.file_not_found_exception), e);
+            Log.e(TAG, getString(R.string.file_not_found_exception) + ": " + file.getAbsolutePath(), e);
+            writeErrorToFile(getString(R.string.file_not_found_exception) + ": " + file.getAbsolutePath(), e);
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception while accessing file: " + file.getAbsolutePath(), e);
+            writeErrorToFile("IO Exception while accessing file: " + file.getAbsolutePath(), e);
         }
     }
 
