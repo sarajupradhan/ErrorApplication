abe17fec97f9e5d8e38fcd858f4a1c51723a0a2f
GAGAN <gagan.patil@zebra.com>
2025-07-15 15:59:05 +0530

    Fix NumberFormatException in BroadcastReceiver by validating input before parsing

--- app/src/main/java/com/example/errorapplication/SamplePTTPro.java
+++ app/src/main/java/com/example/errorapplication/SamplePTTPro.java
@@ -96,3 +96,4 @@ public class SamplePTTPro extends AppCompatActivity {
         registerReceiver(provisioningReceiver, provFilter,RECEIVER_EXPORTED);
     }
 }
+// Timestamp: 2025-07-15T15:59:05.064496
